f0a01332aa64e75ab09ae36e439072c441b0ea1c
Natsuko Haw <natsukohaw@Natsukos-iMac.local>
2020-09-08 15:30:16 -0400

    Adding intro initial dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ const browserSync = require('browser-sync').create();
 // compile scss to css
 function style(){
   // 1. find scss
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src('./assets/scss/styles.scss')
   // 2. pass file through sass compiler
-  //    and clean up error in terminal if needed
+  //    and if error, clean up the message that is outputted to terminal
   .pipe(sass().on('error', sass.logError))
   // 3. save compiled css
-  .pipe(gulp.dest('./css'))
+  .pipe(gulp.dest('./assets/css/'))
   // 4. stream changes to browsers
   .pipe(browserSync.stream());
 }
@@ -21,9 +21,9 @@ function watch(){
       baseDir: './'
     }
   });
-  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./assets/scss/**/*.scss', style);
   gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
