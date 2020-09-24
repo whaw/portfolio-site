0deb053b89c71cc356890f5b5b3fff8bbc123f85
whaw <natsukohaw@Natsukos-iMac.local>
2020-09-24 15:45:26 -0400

    Adding skills animation controller

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 
 $(document).ready(function(){
 
+  // MAIN NAV
+  // animate link scroll
   $(".nav-link").click(function () {
     var id = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(id).offset().top
     }, 'slow');
   });
-
-  // resolve for padding in intro text section throwing off scrollspy nav highlighting
+  // resolve for padding in Introductions section body text throwing off scrollspy nav highlighting
   $('body').scrollspy({
     offset: 50
   });
--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,28 +1,65 @@
-var Skills = {
-  animElements: ['js_skills'],
-  init: function(){
-    this.addAnimElements();
-    this.render();
-  },
-  addAnimElements: function(){
-    this.animElements.forEach(element => {
-      var objectProp = this.convertName(element);
-      Skills[objectProp] = $.makeArray($('.' + element));
-    });
-  },
-  convertName: function(element){
-    return '$' + element.replace('js_', '');
-  },
-  render: function(){
-   this.animate(this.$skills);
-  },
-  animate: function(arr){
-    arr.forEach((element, i) => {
-      $(element).addClass('animate');
-    });
+(function(){
+  var Skills = {
+    hasPlayed: false,
+    animElements: ['js_skills'],
+    init: function(){
+      this.addAnimElements();
+      this.bindEvents();
+      this.render();
+    },
+    addAnimElements: function(){
+      this.animElements.forEach(el => {
+        var prop = this.convertName(el);
+        this.cacheDom(prop, el);
+      });
+    },
+    convertName: function(el){
+      return '$' + el.replace('js_', '');
+    },
+    cacheDom: function(prop, el){
+      this[prop] = $.makeArray($('.' + el));
+    },
+    bindEvents: function(){
+      window.addEventListener('scroll',(e) => {
+        this.isShowing()
+      });
+    },
+    isShowing: function(){
+      var scrollPos = window.scrollY;
+    },
+    render: function(){
+     this.animate(this.$skills);
+    },
+    animate: function(arr){
+      arr.forEach((el) => {
+        $(el).addClass('animate');
+      });
+    }
   }
-}
+  
+  $('document').ready(function(){
+    function check(){
+      if (Skills.hasPlayed == false){
+        var top_of_screen = $(window).scrollTop();
+        var top_of_element = $(".js_skills__content").offset().top;
+        var bottom_of_screen = top_of_screen + $(window).innerHeight();
+        var elementHeight = $(".js_skills__content").outerHeight();
+        var threeQuartersHeight = top_of_element + (elementHeight * .75);
 
-$('document').ready(function(){
-  Skills.init();
-});
+        if ((bottom_of_screen > threeQuartersHeight) && (top_of_screen < top_of_element)){
+            Skills.hasPlayed = true;
+            setTimeout(function(){
+              Skills.init();
+            }, 1500);
+        }
+      }
+    }
+    check();
+
+    $(window).scroll(function() {
+      check();
+    });
+
+  });
+})()
+
